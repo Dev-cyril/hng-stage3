20e3b221e682f94fc0d31076df8ac1fae93b8757
Cyril <cyril.idu2@gmail.com>
2023-09-21 06:59:38 +0100

    added few images

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ export default function App() {
           {
             auth.token ?
             <Route path='/gallery' element={<Gallery />} /> :
-            <Route path="/" element={<SignIn />} />
+            <Route path="*" element={<Navigate to="/" />} />
           }
         </Routes>
       </Router>
